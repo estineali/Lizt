8529e5af7b78468a401afc7fb5cdabf7967ff462
Muhammad Shahrom Ali <estineali@gmail.com>
2020-12-16 03:39:01 +0500

    Unity Implementation

---
+++ Assets/Scripts/DefaultText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DefaultText : MonoBehaviour
+{
+    [SerializeField]
+    TMP_FontAsset defaultFont;
+
+    public TMP_FontAsset DefaultFont
+    {
+        get {return defaultFont;}
+    }
+
+    private void Start()
+    {
+        TextMeshProUGUI[] textObjects = GetComponents<TextMeshProUGUI>();
+
+        foreach (TextMeshProUGUI textObject in textObjects)
+        {
+            textObject.font = defaultFont;
+        }
+    }
+
+}
+
